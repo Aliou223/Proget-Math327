07f35062234527058244709cb776e70b9fb60758
Aliou223 <43020629+Aliou223@users.noreply.github.com>
2018-10-04 20:02:11 -0400

    Add files via upload

---
+++ Graph-plot .R
@@ -0,0 +1,4 @@
+ plot(avocadodt$AveragePrice)
+ plot(avocadodt$Large.Bags)
+ plot(avocadodt$Small.Bags)
+ plot(avocadodt$AveragePrice)
---
+++ MIlestone#3 Mean and standard deviation.R
@@ -0,0 +1,12 @@
+ mean(avocadodt$AveragePrice)
+# 1.405978
+mean(avocadodt$Large.Bags)
+# 54338.09
+mean(avocadodt$Small.Bags)
+# 182194.7
+ sd(avocadodt$Small.Bags)
+# 746178.5
+ sd(avocadodt$large.Bags)
+# NA
+ sd(avocadodt$AveragePrice)
+# 0.4026766
